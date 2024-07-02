66c8aab392451e26e2fa6e08c7ef90c3f88ca5c3
arshiya <arshiyanaheed98@gmail.com>
2024-07-01 19:58:27 -0700

    started making the website

---
+++ website.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pickle 
+
+movies_list = pickle.load(open('movues.pkl','rb'))
+movies_list = movies_list['title'].values
+st.title('Movies')
+
+option = st.selectbox( 'What would like to search?', movies_list )
